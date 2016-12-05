e0ebd9677cb2ac75e5f80097f2e28791e4af039a
amirmekktv <amirmekktv@gmail.com>
2016-12-05 11:57:08 -0500

    fully functional v1.00 with some minor problems such as the weather icon is very small in phones and white color isnt nice for all backgrounds

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-var backgrounds = ['https://static.pexels.com/photos/110874/pexels-photo-110874-large.jpeg',
+'use strict';
+const backgrounds = ['https://static.pexels.com/photos/110874/pexels-photo-110874-large.jpeg',
                    'https://static.pexels.com/photos/3768/sky-sunny-clouds-cloudy-large.jpg',
                    'https://static.pexels.com/photos/24475/pexels-photo-24475-large.jpg',
                    'https://static.pexels.com/photos/7865/sunset-wind-wind-farm-clean-energy-large.jpg'] ,
     backgroundsName = ['rainy','sunny','snowy','windy'],
-    icon = ['https://image.freepik.com/free-icon/rain-black-cloud-with-raindrops-falling-down_318-56422.jpg',
-            'https://image.freepik.com/free-icon/sun-and-cloud_318-79049.jpg',
-            'https://image.freepik.com/free-icon/snowflake-winter-shape_318-27531.jpg',
-            'https://image.freepik.com/free-icon/wind_318-71838.jpg'] , 
-    city = document.getElementById('city') ;
+    city = document.getElementById('city'),
+    temp = document.getElementById('temp') ,
+    description = document.getElementById('description');
 
-  $.getJSON('http://freegeoip.net/json/?callback=?' , function(data){
-    position = data ;
-    latitude = position.latitude ;
-    longitude = position.longitude;
+  $.getJSON('http://freegeoip.net/json/?callback=?' , function(data){ 
+    // getting user position I did not html5 geolocation because if u r on chrome you must have an https connection to use it
+    var position = data , latitude = position.latitude , longitude = position.longitude;
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?APPID=318269c767201abe051535fdaa125266&lat='+ latitude + '&lon=' + longitude+'&units=metric',
+    // sending the retrieved position to open weather service to get the actuel weather 
               function(weatherdata){
-      var stringified = JSON.stringify(weatherdata) ;
-      var parsed = JSON.parse(stringified) ;
-      city.innerHTML = parsed.main.temp ;
+      const stringifiedData = JSON.stringify(weatherdata) ,
+          parsedData = JSON.parse(stringifiedData) ,
+      // making the retrived JSON file usable by stringifing then parsing the file
+          iconURL = "http://openweathermap.org/img/w/"+ parsedData.weather[0].icon +".png";
+      /// the open weather map offer pack of icons available according to the weather so I just used it instead
+      console.log(parsedData) ;
+      temp.innerHTML = parsedData.main.temp +'º C' ;
+      city.innerHTML = parsedData.name +' , '+parsedData.sys.country ;
+      description.innerHTML = parsedData.weather[0].description ;
+      $('#icon').attr({'src' : iconURL ,'alt' : parsedData.weather[0].description ,
+                       'title' :parsedData.weather[0].description  });
+      if(parsedData.weather[0].description === 'light rain' || 'rain' || 'thunderstorm' ){
+        $('#all').css('background-image', `url(" ${backgrounds[0]}")`)}
+        else if(parsedData.weather[0].description === 'clear sky' || 'few clouds '){
+          $('#all').css('background-image', `url(" ${backgrounds[1]}")`)
+        }
+      else if(parsedData.weather[0].description === 'snow'){
+          $('#all').css('background-image', `url(" ${backgrounds[2]}")`)
+        }
+      else if(parsedData.weather[0].description === 'scattered clouds' || 'broken clouds'){
+          $('#all').css('background-image', `url(" ${backgrounds[3]}")`)
+        }
+  
     })
 
   });
